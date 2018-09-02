52097d154bd7c6c8be24211dfba5e12f1e33d2d9
Bob Saska <bob@agrible.com>
2018-09-02 00:57:32 -0500

    Adding pipfile and pm2 ecosystem files

---
+++ ecosystem.config.js
@@ -0,0 +1,35 @@
+module.exports = {
+  /**
+   * Application configuration section
+   * http://pm2.keymetrics.io/docs/usage/application-declaration/
+   */
+  apps : [
+
+    // First application
+    {
+      name      : 'exraider',
+      script    : 'pipenv run start',
+      env: {
+      },
+      env_production : {
+        NODE_ENV: 'production'
+      }
+    },
+  ],
+
+  /**
+   * Deployment section
+   * http://pm2.keymetrics.io/docs/usage/deployment/
+   */
+  deploy : {
+    production : {
+      user : 'ubuntu',
+      host : 'u.r35.net',
+      ref  : 'origin/master',
+      repo : 'git@github.com:r35krag0th/exraidbot.git',
+      path : '/home/ubuntu/workspace/bots/exraidbot',
+      'pre-setup': 'pip install pipenv',
+      'post-deploy' : 'pipenv install && pm2 reload ecosystem.config.js --env production'
+    }
+  }
+};
